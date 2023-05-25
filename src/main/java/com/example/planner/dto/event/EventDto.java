18cfbe441e81f2ee7f7262f0757aaddd2be77512
Sokolanov Matvey <msokolanov@gmail.com>
2023-05-25 19:53:27 +0700

    STUD-18: Добавил CRUD для событий

---
+++ src/main/java/com/example/planner/dto/event/EventDto.java
@@ -0,0 +1,45 @@
+package com.example.planner.dto.event;
+
+import com.example.planner.entities.UserEntity;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+public class EventDto {
+    String id;
+    private String name;
+    private String description;
+    private Date date;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
---
+++ src/main/java/com/example/planner/validators/EventValidator.java
@@ -0,0 +1,18 @@
+package com.example.planner.validators;
+
+import com.example.planner.dto.event.CreateEventDto;
+import com.example.planner.dto.event.EventDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EventValidator {
+
+    public void validateCreateEvent(CreateEventDto createEventDto) {
+
+    }
+
+    public void validateEditEvent(EventDto eventDto) {
+
+    }
+
+}
